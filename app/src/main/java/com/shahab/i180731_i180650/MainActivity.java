f9c60164bfca30aa2dcc04d9be828acf0fcaafdd
gitfudge <piyush.awr@gmail.com>
2021-10-10 15:47:27 +0500

    Start Page And Login Page

--- app/src/main/java/com/shahab/i180731_i180650/MainActivity.java
+++ app/src/main/java/com/shahab/i180731_i180650/MainActivity.java
@@ -2,13 +2,36 @@ package com.shahab.i180731_i180650;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    public int counter = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        new CountDownTimer(1000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+                counter++;
+
+            }
+
+            @Override
+            public void onFinish() {
+                Intent StartIntent = new Intent(MainActivity.this, LoginPageActivity.class);
+                startActivity(StartIntent);
+            }
+        }.start();
     }
+
 }
