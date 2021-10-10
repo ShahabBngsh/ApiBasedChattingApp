12794d5d1ff4ed674efa82443c5394a26550df62
Shahab-Bangash <shahab@paalam.co.uk>
2021-10-10 10:38:43 +0500

    first screen is now clickable, It will lead to the login page

--- app/src/main/java/com/shahab/i180731_i180650/MainActivity.java
+++ app/src/main/java/com/shahab/i180731_i180650/MainActivity.java
@@ -2,13 +2,30 @@ package com.shahab.i180731_i180650;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
-
+    LinearLayout btn_ll_main;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_ll_main = findViewById(R.id.main);
+        btn_ll_main.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchActivityLogin();
+            }
+        });
     }
+    // switch screen to the login screen
+    private void launchActivityLogin() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
 }
