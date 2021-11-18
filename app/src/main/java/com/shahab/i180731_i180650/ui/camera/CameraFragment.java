c2cae3a44eadc0a9b8ff1246e349e4cd211ddfdd
ShahabBngsh <shahabbng1999@gmail.com>
2021-11-18 16:15:16 +0500

    select multiple images from gallery fixed

--- app/src/main/java/com/shahab/i180731_i180650/ui/camera/CameraFragment.java
+++ app/src/main/java/com/shahab/i180731_i180650/ui/camera/CameraFragment.java
@@ -1,15 +1,22 @@
 package com.shahab.i180731_i180650.ui.camera;
 
+import static android.app.Activity.RESULT_OK;
+
 import android.app.Activity;
+import android.content.ClipData;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -20,7 +27,14 @@ import androidx.lifecycle.ViewModelProvider;
 import com.shahab.i180731_i180650.R;
 import com.shahab.i180731_i180650.databinding.FragmentCameraBinding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CameraFragment extends Fragment {
+
+    int PICK_IMAGE_MULTIPLE = 1;
+    String imageEncoded;
+    List<String> imagesEncodedList;
 
     private CameraViewModel cameraViewModel;
     private FragmentCameraBinding binding;
@@ -45,25 +59,76 @@ public class CameraFragment extends Fragment {
             }
         });
 
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent,"Select Picture"), PICK_IMAGE_MULTIPLE);
 
-        final ImageView img_cap = binding.cameraResult;
-        Intent StartIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        this.startActivityForResult(StartIntent,pic_id);
+//        final ImageView img_cap = binding.cameraResult;
+//        Intent StartIntent = new Intent(MediaStore.);
+//        this.startActivityForResult(StartIntent,pic_id);
         return root;
 
     }
-
+    //https://stackoverflow.com/questions/19585815/select-multiple-images-from-android-gallery
     @Override
-    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        try {
+            // When an Image is picked
+            if (requestCode == PICK_IMAGE_MULTIPLE && resultCode == RESULT_OK
+                    && null != data) {
+                // Get the Image from data
 
-        if (requestCode == pic_id && resultCode == Activity.RESULT_OK) {
+                String[] filePathColumn = { MediaStore.Images.Media.DATA };
+                imagesEncodedList = new ArrayList<String>();
+                if(data.getData()!=null){
 
-                    Bitmap photo = (Bitmap) data.getExtras().get("data");
-                    binding.cameraResult.setImageBitmap(photo);
+                    Uri mImageUri=data.getData();
+
+                    // Get the cursor
+                    Cursor cursor = getActivity().getContentResolver().query(mImageUri,
+                            filePathColumn, null, null, null);
+                    // Move to first row
+                    cursor.moveToFirst();
+
+                    int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                    imageEncoded  = cursor.getString(columnIndex);
+                    cursor.close();
+
+                } else {
+                    if (data.getClipData() != null) {
+                        ClipData mClipData = data.getClipData();
+                        ArrayList<Uri> mArrayUri = new ArrayList<Uri>();
+                        for (int i = 0; i < mClipData.getItemCount(); i++) {
+
+                            ClipData.Item item = mClipData.getItemAt(i);
+                            Uri uri = item.getUri();
+                            mArrayUri.add(uri);
+                            // Get the cursor
+                            Cursor cursor = getActivity().getContentResolver().query(uri, filePathColumn, null, null, null);
+                            // Move to first row
+                            cursor.moveToFirst();
+
+                            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                            imageEncoded  = cursor.getString(columnIndex);
+                            imagesEncodedList.add(imageEncoded);
+                            cursor.close();
+
+                        }
+                        Log.v("LOG_TAG", "Selected Images" + mArrayUri.size());
+                    }
+                }
+            } else {
+                Toast.makeText(getContext(), "You haven't picked Image",
+                        Toast.LENGTH_LONG).show();
+            }
+        } catch (Exception e) {
+            Toast.makeText(getContext(), "Something went wrong", Toast.LENGTH_LONG)
+                    .show();
         }
 
-
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
 
